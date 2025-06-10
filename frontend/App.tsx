241ad61ac0db3a344caf225267972c29cf35053b
Mahdi <no-reply@you.com>
2025-06-10 11:22:29 -0400

    Remove Testimonials component
    - Remove Testimonials component
    - Remove Testimonials.tsx file
    - Remove Testimonials.tsx file

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,7 +1,6 @@
 import { Hero } from './components/Hero';
 import { Services } from './components/Services';
 import { About } from './components/About';
-import { Testimonials } from './components/Testimonials';
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
 
@@ -11,7 +10,6 @@ export default function App() {
       <Hero />
       <Services />
       <About />
-      <Testimonials />
       <Contact />
       <Footer />
     </div>
